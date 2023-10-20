38f56ebd70ef484c376f2d621ba772cec5450b95
Hoda Elnaghy <hhesham2582000@gmail.com>
2023-10-20 17:04:52 +0300

    Add the project missing files

---
+++ MagicaVogue/Cart/Components/CartCell.swift
@@ -0,0 +1,60 @@
+//
+//  CartCell.swift
+//  MagicaVogue
+//
+//  Created by Heba Elcc on 18.10.2023.
+//
+
+import UIKit
+
+class CartCell: UITableViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    @IBOutlet weak var minus: UIButton!
+    
+    @IBOutlet weak var plus: UIButton!
+    
+    var quantity: Int = 1 {
+        didSet {
+            // Update the quantity label when the quantity changes
+            quantityLabel.text = "\(quantity)"
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        productImageView.layer.cornerRadius = 10
+        productImageView.clipsToBounds = true
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        print("Plus button pressed")
+        quantity += 1
+        print("New quantity: \(quantity)")
+    }
+    
+    @IBAction func didPressMinus(_ sender: Any) {
+        print("Minus button pressed")
+        if quantity > 0 {
+            quantity -= 1
+            print("New quantity: \(quantity)")
+        }
+    }
+    
+    
+    
+}
---
+++ MagicaVogue/Cart/Model/CartModel.swift
@@ -0,0 +1,16 @@
+//
+//  CartModel.swift
+//  MagicaVogue
+//
+//  Created by Shimaa Elcc on 18.10.2023.
+//
+
+//import Foundation
+//import UIKit
+//
+//struct CartItem {
+//       let image: UIImage
+//       let name: String
+//       let price: Double
+//       var quantity: Int
+//   }
