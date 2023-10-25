7fb5945019665be6a3a15fd01989991615c47fa2
Hoda Elnaghy <hhesham2582000@gmail.com>
2023-10-26 02:46:11 +0300

    Merged with coupons

--- MagicaVogue/SuppotingFiles/SceneDelegate.swift
+++ MagicaVogue/SuppotingFiles/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window = UIWindow(windowScene: windowScene)
 //        let rootViewController = UserProfileViewController(viewModel: UserProfileViewModel())
         
-        let rootViewController = UserLoginViewController()
+        let rootViewController = TabBarController()
 
             window?.rootViewController = UINavigationController(rootViewController: rootViewController)
             window?.makeKeyAndVisible()
