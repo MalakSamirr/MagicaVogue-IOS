ec4278143631562e9aab474e3500ac84ad1c9430
MalakSamirr <malak.samir2001@gmail.com>
2023-10-27 14:45:30 +0200

    start getting product details in product details view controller

--- MagicaVogue/Module/ProductDetails/View/ProductDetailsViewController.swift
+++ MagicaVogue/Module/ProductDetails/View/ProductDetailsViewController.swift
@@ -4,17 +4,19 @@
 //
 //  Created by Malak Samir on 18/10/2023.
 //
-
 import UIKit
+import Kingfisher
 
 class ProductDetailsViewController: UIViewController , UICollectionViewDelegate,UICollectionViewDataSource , UICollectionViewDelegateFlowLayout ,UIScrollViewDelegate {
     
-
+    
+    
+    
     @IBOutlet weak var sliderControlPage: UIPageControl!
     var currentCell = 0
     private var timer: Timer?
     
-    let arrOfImgs = ["3","1","2"]
+    var arrOfProductImgs : [String] = []
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -27,6 +29,13 @@ class ProductDetailsViewController: UIViewController , UICollectionViewDelegate,
     
     @IBOutlet weak var large: UIButton!
     
+    
+    @IBOutlet weak var productName: UILabel!
+    
+    @IBOutlet weak var productDetails: UILabel!
+    
+    @IBOutlet weak var productPrice: UILabel!
+    
     var myProduct : Products!
     
     
@@ -37,7 +46,14 @@ class ProductDetailsViewController: UIViewController , UICollectionViewDelegate,
         
         print (myProduct.id)
         print("-----------malak---------")
-
+        for image in myProduct.images!{
+            arrOfProductImgs.append(image.src ?? "")
+        }
+        
+        productPrice.text = "EG\(myProduct.variants?[0].price ?? "0")"
+        
+        productName.text = myProduct.title
+        productDetails.text = myProduct.body_html
         
         scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: 1000)
         
@@ -48,63 +64,67 @@ class ProductDetailsViewController: UIViewController , UICollectionViewDelegate,
         layout.itemSize = CGSize(width:collectionView.frame.width , height: 270)
         layout.minimumLineSpacing = 10
         layout.minimumInteritemSpacing = 10
-        layout.scrollDirection = .horizontal 
+        layout.scrollDirection = .horizontal
         collectionView.setCollectionViewLayout(layout, animated: false)
         
         automaticSlide()
         setupPageControl()
         collectionView?.isPagingEnabled = true
-                collectionView?.decelerationRate = .fast
-
+        collectionView?.decelerationRate = .fast
+        
     }
     private func setupPageControl() {
-            sliderControlPage?.hidesForSinglePage = true
-            sliderControlPage?.addTarget(self, action: #selector(pageControlHandle), for: .valueChanged)
+        sliderControlPage?.hidesForSinglePage = true
+        sliderControlPage?.addTarget(self, action: #selector(pageControlHandle), for: .valueChanged)
+    }
+    
+    @objc private func pageControlHandle(sender: UIPageControl) {
+        let indexPath = IndexPath(row: sender.currentPage, section: 0)
+        if let frame = collectionView?.layoutAttributesForItem(at: indexPath)?.frame {
+            collectionView?.scrollRectToVisible(frame, animated: true)
         }
-        
-        @objc private func pageControlHandle(sender: UIPageControl) {
-            let indexPath = IndexPath(row: sender.currentPage, section: 0)
-            if let frame = collectionView?.layoutAttributesForItem(at: indexPath)?.frame {
-                collectionView?.scrollRectToVisible(frame, animated: true)
-            }
+    }
+    
+    private func automaticSlide () {
+        timer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(slide), userInfo: nil, repeats: true)
+    }
+    
+    @objc func slide() {
+        if currentCell < arrOfProductImgs.count - 1 {
+            currentCell += 1
+        } else {
+            currentCell = 0
         }
-        
-        private func automaticSlide () {
-            timer = Timer.scheduledTimer(timeInterval: 2.5, target: self, selector: #selector(slide), userInfo: nil, repeats: true)
+        sliderControlPage.currentPage = currentCell
+        let indexPath = IndexPath(row: sliderControlPage.currentPage, section: 0)
+        if let frame = collectionView?.layoutAttributesForItem(at: indexPath)?.frame {
+            collectionView?.scrollRectToVisible(frame, animated: true)
         }
-        
-        @objc func slide() {
-            if currentCell < arrOfImgs.count - 1 {
-                currentCell += 1
-            } else {
-                currentCell = 0
-            }
-            sliderControlPage.currentPage = currentCell
-            let indexPath = IndexPath(row: sliderControlPage.currentPage, section: 0)
-            if let frame = collectionView?.layoutAttributesForItem(at: indexPath)?.frame {
-                collectionView?.scrollRectToVisible(frame, animated: true)
-            }
-        }
+    }
     
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-            let pageNumber = scrollView.contentOffset.x / scrollView.frame.width
-            sliderControlPage?.currentPage = Int(ceil(pageNumber))
-        }
-        func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-            sliderControlPage?.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
-        }
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.width
+        sliderControlPage?.currentPage = Int(ceil(pageNumber))
+    }
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        sliderControlPage?.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return arrOfProductImgs.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductImageCell" , for: indexPath) as! ProductImageCell
         
-        cell.img.image = UIImage(named: arrOfImgs[indexPath.row])
+        
+        let img = arrOfProductImgs[indexPath.row]
+        if let imageUrl = URL(string: img) {
+            cell.img.kf.setImage(with: imageUrl)
+        }
         return cell
+        
+        
     }
-    
-    
 }
